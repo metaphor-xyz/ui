8819d3084c7bb4f4bf2b574d894cf56b10374648
Gail Wilson <gail.de.wilson@gmail.com>
2021-12-31 14:21:40 -0500

    remove files

--- dist/ProfileComponent.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-interface ProfileComponentProps {
-    name: string;
-    address: string;
-    onClick?: () => void;
-}
-export default function ProfileComponent({ name, address, onClick }: ProfileComponentProps): JSX.Element;
-export {};
