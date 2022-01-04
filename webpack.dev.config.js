5a1622a948a8168161ee449514550bd4d3def212
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-01-04 11:03:00 -0600

    switch to webpack build, add file-loader

---
+++ webpack.dev.config.js
@@ -0,0 +1,9 @@
+const prodConfig = require('./webpack.config');
+const merge = (...objs) => require('deepmerge').all(objs, {arrayMerge: (arr1, arr2) => arr1.concat(arr2) });
+
+const combinedConfig = merge({}, prodConfig, {
+  mode: 'development',
+  watch: true,
+});
+
+module.exports = combinedConfig;
